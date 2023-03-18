0412286894648c622ce93f89412edd7cea99367d
Óscar Tarazaga Gálvez <otarazagagalvez@safareyes.es>
2023-03-19 00:04:46 +0100

    -

---
+++ V1/Paciente_citamedica.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" type="text/css" href="CSS/pacientecss.css">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Horario</title>
+</head>
+<body>
+    <h1>Eliga la hora a la que desea la consulta</h1>
+    <!--<Tabla con horario disponible para elegir la cita>-->
+</body>
+</html>
---
+++ V1/paciente.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="CSS/pacientecss.css">
+    <title>Panel del paciente</title>
+</head>
+<body>
+    <div>
+        <h1>Bienvenido a la interfaz del usuario</h1>
+        <h2>¿Qué desea hacer?</h2>
+    <form>
+        <input type="radio" id="horario" name="opcion" value="horario">
+        <label for="horario"> Cita Médica</label><br>
+        <input type="radio" id="PanelControl" name="opcion" value="PanelControl">
+        <label for="PanelControl"> Receta médica</label><br>
+        <input type="radio" id="infopaciente" name="opcion" value="PanelControl">
+        <label for="PanelControl"> Información del usuario</label>
+    </form>
+    </div>
+</body>
+</html>
