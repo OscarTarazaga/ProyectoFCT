8918c91ace7c993dcd96229e0194687c5e302e93
Óscar Tarazaga Gálvez <otarazagagalvez@safareyes.es>
2023-03-25 14:24:44 +0100

    Update index.php

--- V1/index.php
+++ V1/index.php
@@ -5,7 +5,6 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" type="text/css" href="CSS/indexcss.css">
-
     <title>Pagina Principal</title>
 </head>
 <body>
@@ -14,11 +13,12 @@
         <form action="/action_page.php">
             <label for="usuario">Usuario:</label><br>
             <input type="text" id="usuario" name="usuario" placeholder="Nombre del usuario"> <br>
+            <label for="DNI">DNI:</label><br>
+            <input type="text" id="DNI" name="DNI" placeholder="DNI del usuario"> <br>
             <label for="passwd">Contraseña:</label><br>
             <input type="text" id="passwd" name="passwd" placeholder="contraseña"><br><br>
             <input type="submit" value="Login">
          </form> 
     </div>
-
 </body>
 </html>
