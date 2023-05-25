d1fc67ca04fccb29f7172f18bd06e2d47ceb138c
Óscar Tarazaga Gálvez <otarazagagalvez@safareyes.es>
2023-05-25 18:18:09 +0200

    -

--- V1/medico_horario.php
+++ V1/medico_horario.php
@@ -5,16 +5,25 @@ session_start();
 $horario_inicio = "";
 $horario_fin = "";
 // Habilitamos la conexión con la base de datos
-$host="127.0.0.1";
-$port=3306;
-$user="root";
-$password="root";
-$dbname="proyectofct";
+$host = "127.0.0.1";
+$port = 3306;
+$user = "root";
+$password = "root";
+$dbname = "proyectofct";
 
 $conexion = mysqli_connect($host, $user, $password, $dbname, $port);
 
+// Definimos un array asociativo para traducir los días de la semana de inglés a español
+$dias_semana = array(
+    "Monday" => "Lunes",
+    "Tuesday" => "Martes",
+    "Wednesday" => "Miércoles",
+    "Thursday" => "Jueves",
+    "Friday" => "Viernes"
+);
+
 // Comprobamos si el usuario ha iniciado sesión correctamente
-if(!isset($_SESSION['dni'])){
+if (!isset($_SESSION['dni'])) {
     header("Location: medico_login.php");
     exit;
 }
@@ -25,7 +34,7 @@ $dni = $_SESSION['dni'];
 $query = "SELECT horario_inicio, horario_fin FROM doctores WHERE dni='$dni'";
 $result = mysqli_query($conexion, $query);
 
-if(mysqli_num_rows($result) == 1){
+if (mysqli_num_rows($result) == 1) {
     $row = mysqli_fetch_assoc($result);
     $horario_inicio = $row['horario_inicio'];
     $horario_fin = $row['horario_fin'];
@@ -34,24 +43,29 @@ if(mysqli_num_rows($result) == 1){
     exit;
 }
 
-// Consultamos las citas asignadas al médico
+// Consultamos las citas asignadas al médico para la semana actual
+$fecha_actual = date("Y-m-d");
+$fecha_inicio_semana = date("Y-m-d", strtotime("last Monday", strtotime($fecha_actual)));
+$fecha_fin_semana = date("Y-m-d", strtotime("next Friday", strtotime($fecha_actual)));
 $query_citas = "SELECT cita.hora, cita.dia, pacientes.nombre, pacientes.apellidos FROM cita 
                 INNER JOIN pacientes ON cita.dni_paciente = pacientes.dni
-                WHERE cita.dni_doctor='$dni'";
+                WHERE cita.dni_doctor='$dni' AND cita.dia BETWEEN '$fecha_inicio_semana' AND '$fecha_fin_semana'";
 $result_citas = mysqli_query($conexion, $query_citas);
-
 
 // Creamos un array asociativo para almacenar las citas
 $citas = array();
 while ($row_citas = mysqli_fetch_assoc($result_citas)) {
-    $citas[$row_citas['dia']] = $row_citas['hora'];
+    $dia = $row_citas['dia'];
+    $hora = $row_citas['hora'];
+    $citas[$dia][$hora] = $row_citas['nombre'] . " " . $row_citas['apellidos'];
 }
 
 mysqli_close($conexion);
 ?>
 
 <!DOCTYPE html>
-<html lang="en">
+<html lang="es">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -62,42 +76,45 @@ mysqli_close($conexion);
 elementos asociados al css.
 En este caso, he definido que el titulo salga en el centro del div, y el formato de la tabla, para que salga dentro de celdas, excepto la primera celda de todas, ya que esta 
 aparece sin borde alguno>-->
-
 </head>
 <body>
-<!--<Aquí definimos la tabla>-->
     <div>
         <h1>Horario del doctor</h1>
         <table>
             <tr>
                 <th class="a"></th>
-                <th>Lunes</th>
-                <th>Martes</th>
-                <th>Miércoles</th>
-                <th>Jueves</th>
-                <th>Viernes</th>
+                <th><?php echo $dias_semana[date("l", strtotime($fecha_inicio_semana))]; ?></th>
+                <th><?php echo $dias_semana[date("l", strtotime($fecha_inicio_semana . " +1 day"))]; ?></th>
+                <th><?php echo $dias_semana[date("l", strtotime($fecha_inicio_semana . " +2 days"))]; ?></th>
+                <th><?php echo $dias_semana[date("l", strtotime($fecha_inicio_semana . " +3 days"))]; ?></th>
+                <th><?php echo $dias_semana[date("l", strtotime($fecha_inicio_semana . " +4 days"))]; ?></th>
             </tr>
             <?php
-            // Genera cada fila de la tabla usando como inicio la hora actual que es la variable horario_inicio y usando tambien el dia actual, esto para poder
+            // Genera cada fila de la tabla usando como inicio la hora actual que es la variable horario_inicio y usando también el día actual, esto para poder
             // generar las celdas en cada día de la semana desde el lunes hasta el viernes.
             $hora_actual = $horario_inicio;
-            $dia_actual = 1;
             while (strtotime($hora_actual) <= strtotime($horario_fin)) {
                 echo "<tr>";
                 echo "<td>$hora_actual</td>";
+                $fecha_comparar = $fecha_inicio_semana;
                 for ($j = 0; $j < 5; $j++) {
-                    echo "<td></td>";
+                    if (isset($citas[$fecha_comparar][$hora_actual])) {
+                        $cita = $citas[$fecha_comparar][$hora_actual];
+                        echo "<td>$cita</td>";
+                    } else {
+                        echo "<td></td>";
+                    }
+                    $fecha_comparar = date("Y-m-d", strtotime($fecha_comparar . " +1 day"));
                 }
                 echo "</tr>";
                 // Esto hace que se imprima el horario en cada celda con una diferencia de media hora.
                 $hora_actual = date('H:i:s', strtotime("$hora_actual + 30 minutes"));
-                $dia_actual++;
             }
             ?>
         </table>
     </div>
-        <form action="medico.php "  class="volver-btn" method="Post">
-            <input type="submit" value="Volver a la seleccion">
-        </form>
+    <form action="medico.php" class="volver-btn" method="Post">
+        <input type="submit" value="Volver a la selección">
+    </form>
 </body>
 </html>
